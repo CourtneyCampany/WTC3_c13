817381dab231aaa4c510622f9c7a90f9205f0851
Courtney Campany <courtneycampany@gmail.com>
2017-03-07 10:09:11 +1100

    started flux plotting, sent email to john to get started

---
+++ scripts/cham_flux_plot.R
@@ -0,0 +1,19 @@
+
+# read in chamber fllux data ----------------------------------------------
+
+chamflux <- read.csv("data/chamberflux_15m.csv")
+library(lubridate)  
+chamflux$date <- as.Date(chamflux$date, tz='Australia/Sydney')
+chamflux$datetime <- ymd_hms(chamflux$datetime, tz='Australia/Sydney')
+chamflux$id <- paste(chamflux$campaign, chamflux$day, sep="-")
+
+# simple plots ------------------------------------------------------------
+
+chamlist <- ls()
+
+chamlist <- split(chamflux, "id")
+
+
+plot(FluxCO2 ~ datetime, col=chamber, data=chamflux[chamflux$id == "4-2",])
+
+
