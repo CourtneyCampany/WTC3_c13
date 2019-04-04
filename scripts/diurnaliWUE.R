baafe626e1b08f619286d055859ca0f3fe266148
Teresa E. Gimeno <tegimeno@gmail.com>
2019-04-04 18:49:04 +0200

    I think these are the good correlations and corrections for iWUE

--- scripts/diurnaliWUE.R
+++ scripts/diurnaliWUE.R
@@ -1,17 +1,7 @@
 campaigns <- c('Oct','Dec','Jan','Feb','Mar','Apr')
-
+myPch <- c(15, 16, 17, 18, 19, 8)
 myPalEle <- c('red', 'darkorange1', 'deeppink', 'yellow', 'chocolate4', 'darkgoldenrod1')
 myPalAmb <- c('blue', 'cornflowerblue', 'navyblue', 'cyan', 'darkturquoise', 'deepskyblue')
-
-campsA <- list()
-for(i in 1:length(campaigns)){
-  campsA[[i]] <- subset(allPaired, month==campaigns[i] & T_treatment=='ambient')
-}
-campsE <- list()
-for(i in 1:length(campaigns)){
-  campsE[[i]] <- subset(allPaired, month==campaigns[i] & T_treatment=='warmed')
-}
-myPch <- c(15, 16, 17, 18, 19, 8)
 
 # diurnal gmes
 windows(15,8)
@@ -20,37 +10,56 @@ par(mar=c(7,7,2,2))
 
 plotGmes <- function(x, campaigns, myLevel, myCol, myPch){
   k <- subset(x, month==campaigns[1] & T_treatment==myLevel)
-  plot(k$gmes_area~k$Time, ylim=c(0,1.1), pch=myPch[1], col=myCol,
+  plot(k$gmes_area~k$Time, ylim=c(0,1.1), pch=myPch[1], col=myCol[1],
        main=k$T_treatment[1], xlim=c(5,19), cex.lab=1.8,
        xlab='Time (h)', ylab=expression(italic(g)[mes]~(mol~m^-2~s^-1)))
   legend('topleft', legend=campaigns, pch=myPch, col='black', bty='n')
   for (i in 2:length(campaigns)){
     points(subset(x, month==campaigns[i] & T_treatment==myLevel)[,'gmes_area']~
-             subset(x, month==campaigns[i] & T_treatment==myLevel)[,'Time'], pch=myPch[i], col=myCol)
+             subset(x, month==campaigns[i] & T_treatment==myLevel)[,'Time'], pch=myPch[i], col=myCol[i])
     }
 }
 
-plotGmes(allPaired, campaigns = campaigns, myLevel='ambient', myCol='blue', myPch = myPch)
-plotGmes(allPaired, campaigns = campaigns, myLevel='warmed', myCol='red', myPch = myPch)
+plotGmes(allPaired, campaigns = campaigns, myLevel='ambient', myCol=myPalAmb, myPch = myPch)
+plotGmes(allPaired, campaigns = campaigns, myLevel='warmed', myCol=myPalEle, myPch = myPch)
 
 plotGmes <- function(x, campaigns, myLevel, myCol, myPch){
   k <- subset(x, month==campaigns[1] & T_treatment==myLevel)
-  plot(log(k$gmes_area*1000)~k$Time, ylim=c(-1.3,7), pch=myPch[1], col=myCol,
+  plot(log(k$gmes_area*1000)~k$Time, ylim=c(-1.3,7), pch=myPch[1], col=myCol[1],
        main=k$T_treatment[1], xlim=c(5,19), cex.lab=1.8,
        xlab='Time (h)', ylab=expression(Lg~(italic(g)[mes]~mmol~m^-2~s^-1)))
   legend('topleft', legend=campaigns, pch=myPch, col='black', bty='n')
   for (i in 2:length(campaigns)){
     points(log(subset(x, month==campaigns[i] & T_treatment==myLevel)[,'gmes_area']*1000)~
-             subset(x, month==campaigns[i] & T_treatment==myLevel)[,'Time'], pch=myPch[i], col=myCol)
+             subset(x, month==campaigns[i] & T_treatment==myLevel)[,'Time'], pch=myPch[i], col=myCol[i])
   }
 }
-plotGmes(allPaired, campaigns = campaigns, myLevel='ambient', myCol='blue', myPch = myPch)
-plotGmes(allPaired, campaigns = campaigns, myLevel='warmed', myCol='red', myPch = myPch)
+plotGmes(allPaired, campaigns = campaigns, myLevel='ambient', myCol=myPalAmb, myPch = myPch)
+plotGmes(allPaired, campaigns = campaigns, myLevel='warmed', myCol=myPalEle, myPch = myPch)
 
 # diurnal iWUEge (Ca-Ci)
 windows(15,8)
-par(mfrow=c(1,2))
+par(mfrow=c(2,2))
 par(mar=c(7,7,2,2))
+
+plotGmes <- function(x, campaigns, myLevel, myCol, myPch){
+  k <- subset(x, month==campaigns[1] & T_treatment==myLevel)
+  plot(k$iWUE~k$Time, ylim=c(0,305), pch=myPch[1], col=myCol[1],
+       main=k$T_treatment[1], xlim=c(5,19), cex.lab=1.8,
+       xlab='Time (h)', ylab=expression(italic(A)[net]/italic(g)[sc]~(mu*mol~mol^-1)))
+  abline(k[which(!is.na(k$iWUEph_corrMD))[1],'iWUEph_corrMD'], 0, col=myCol[1])
+  legend('topleft', legend=campaigns, pch=myPch, col=myCol, bty='n')
+  for (i in 2:length(campaigns)){
+    points(subset(x, month==campaigns[i] & T_treatment==myLevel)[,'iWUE']~
+             subset(x, month==campaigns[i] & T_treatment==myLevel)[,'Time'], pch=myPch[i], col=myCol[i])
+    abline(x[which(!is.na(x$iWUEph_corrMD))[1],'iWUEph_corrMD'], 0, col=myCol[i])
+  }
+}
+
+plotGmes(allPaired[which(!is.na(allPaired$gmes_area)),], campaigns = campaigns, myLevel='ambient',
+         myCol=myPalAmb, myPch = myPch)
+plotGmes(allPaired[which(!is.na(allPaired$gmes_area)),], campaigns = campaigns, myLevel='warmed',
+         myCol=myPalEle, myPch = myPch)
 
 plotGmes <- function(x, campaigns, myLevel, myCol, myPch){
   k <- subset(x, month==campaigns[1] & T_treatment==myLevel)
--- scripts/theGoodiWUEcorr.R
+++ scripts/theGoodiWUEcorr.R
@@ -8,19 +8,42 @@ phl$chamber <- ifelse((nchar(phl$chamber2) == 1), paste0('C0', phl$chamber2), pa
 phl$month <- str_sub(phl$month, 1, 3)
 names(phl)[2] <- 'd13Cph'
 
-
-del13CcampAvg <- doBy::summaryBy(Corrdel13C_Avg + CO2sampleWTC + Cc + diff_Ci.Cc ~ month + chamber,
+del13CcampAvg <- doBy::summaryBy(iWUEge_corr + iWUE + Corrdel13C_Avg + CO2sampleWTC + Cc + diff_Ci.Cc ~ month + chamber,
                                  FUN=mean.na, data=subset(allPaired, PAR > 3 & condAlert=='no'
-                                                          & A_area > 0 & E_area > 0))
-del13CcampAvgMD <- doBy::summaryBy(iWUEge_corr + Corrdel13C_Avg + CO2sampleWTC + diff_Ci.Cc ~ month + chamber, FUN=mean.na,
-                                   data=subset(allPaired, midday=='yes' & condAlert=='no' & A_area > 0 & E_area > 0))
+                                                          & A_area > 0 & E_area > 0 & iWUE < 500))
+del13CcampAvgMD <- doBy::summaryBy(iWUEge_corr + iWUE + Corrdel13C_Avg + CO2sampleWTC + diff_Ci.Cc ~ month + chamber,
+                                   FUN=mean.na, data=subset(allPaired, midday=='yes' & condAlert=='no' &
+                                                              A_area > 0 & E_area > 0& iWUE < 500))
 names(del13CcampAvgMD)[3:ncol(del13CcampAvgMD)] <- paste0(names(del13CcampAvgMD)[3:ncol(del13CcampAvgMD)], 'MD')
-phl <- merge(phl[,c('chamber','month','d13Cph','temp')],
-             merge(del13CcampAvg, del13CcampAvgMD, by=c('month', 'chamber'), all=T),
-             by=c('month','chamber'), all=T)
+phl <- merge(merge(merge(del13CcampAvgMD, del13CcampAvgDay, by=c('month','chamber'), all=T),
+                   del13CcampAvg, by=c('month','chamber'), all=T), phl, by=c('month','chamber'), all=T)
 phl <- merge(phl, leafChem, by=c('month','chamber'), all=T)
 
+chambs <- data.frame(row.names=1:12)
+chambs$chamber <- c(paste0('C0', 1:9), paste0('C', 10:12))
+chambs$T_treatment <- rep(c('ambient','warmed'), 6)
+
+phl <- merge(phl, chambs, by='chamber', all=T)
+
 phl$iWUEph_uncorrMD <- phl$CO2sampleWTC.mean.naMD*(1-((phl$Corrdel13C_Avg.mean.naMD-phl$d13Cph-a)/(b-a)))
+phl$iWUEph_corrMD <- phl$iWUEph_uncorrMD - phl$diff_Ci.Cc.mean.naMD
 phl$iWUEsunLeaf_uncorrMD <- phl$CO2sampleWTC.mean.naMD*(1-((phl$Corrdel13C_Avg.mean.naMD-phl$d13CsunLeaf-a)/(b-a)))
+phl$iWUEsunLeaf_corrMD <- phl$iWUEsunLeaf_uncorrMD - phl$diff_Ci.Cc.mean.naMD
 phl$iWUEshLeaf_uncorrMD <- phl$CO2sampleWTC.mean.naMD*(1-((phl$Corrdel13C_Avg.mean.naMD-phl$d13CshLeaf-a)/(b-a)))
+phl$iWUEshLeaf_corrMD <- phl$iWUEshLeaf_uncorrMD - phl$diff_Ci.Cc.mean.naMD
 phl$iWUEleafAvg_uncorrMD <- phl$CO2sampleWTC.mean.naMD*(1-((phl$Corrdel13C_Avg.mean.naMD-phl$d13CleafAvg-a)/(b-a)))
+phl$iWUEleafAvg_corrMD <- phl$iWUEleafAvg_uncorrMD - phl$diff_Ci.Cc.mean.naMD
+
+summary(lm(iWUE.mean.naMD ~ iWUEph_corrMD, data=phl))
+summary(lm(iWUE.mean.naMD ~ iWUEsunLeaf_corrMD, data=phl))
+summary(lm(iWUE.mean.naMD ~ iWUEshLeaf_corrMD, data=phl))
+summary(lm(iWUE.mean.naMD ~ iWUEleafAvg_corrMD, data=phl))
+summary(lm(iWUEge_corr.mean.naMD ~ iWUEph_uncorrMD, data=phl))
+summary(lm(iWUEge_corr.mean.naMD ~ iWUEleafAvg_uncorrMD, data=phl))
+
+iWUEsumm <- doBy::summaryBy(iWUE.mean.naMD + iWUE.mean.na + iWUEph_corrMD + iWUEph_uncorrMD +
+                              iWUEleafAvg_corrMD ~ month + T_treatment, data=phl, FUN=c(mean.na, s.err.na))
+summary(lm(iWUE.mean.naMD.mean.na ~ iWUEph_corrMD.mean.na, data=iWUEsumm))
+summary(lm(iWUE.mean.naMD.mean.na ~ iWUEph_uncorrMD.mean.na, data=iWUEsumm))
+summary(lm(iWUE.mean.naMD.mean.na ~ iWUEleafAvg_corrMD.mean.na, data=iWUEsumm))
+write.csv(iWUEsumm, file='output/iWUEsumm.csv', row.names = F)
