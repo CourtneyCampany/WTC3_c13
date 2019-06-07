5e39adbe6b7ab38c40080e784e9b89332a211470
Teresa E. Gimeno <tegimeno@gmail.com>
2019-06-07 12:46:28 +0200

    Code for figure S5

--- master_scripts/d13Cph_d13CAnet_boxplot.R
+++ master_scripts/d13Cph_d13CAnet_boxplot.R
@@ -1,3 +1,4 @@
+source('scripts/canopy_gmes_more2.R')
 photoSumm <- dplyr::summarise(dplyr::group_by(setDT(subset(allPaired, midday=='yes' & A_area > 0 & PAR >= 800
                                                            & deltaSubstrate >= -60 & deltaSubstrate <= 0)),
                                               chamber, month), d13CAnet=mean(deltaSubstrate, na.rm=T))
@@ -21,24 +22,28 @@ phloem <- campaignassign(phloem)
 names(phloem)[1] <- 'd13Cph'
 phloem <- merge(phloem, photoSumm, by=c('chamber','month'), all=T)
 chambs <- data.frame(row.names = 1:12)
-chambs$temp <- rep(c('ambient', 'elevated'), 6)
+chambs$temp <- rep(c('ambient', 'warmed'), 6)
 chambs$chamber <- c(paste0('C0', 1:9), paste0('C', 10:12))
 phloem <- merge(phloem, chambs, by='chamber', all=T)
 phloem$id <- as.factor(paste0(phloem$month, '-', phloem$temp))
-
-phloem$id <- levels(phloem3$id, c("Oct-ambient", "Oct-warmed", "Dec-ambient", "Dec-warmed", 
+phloem$id <- factor(phloem$id, levels=c("Oct-ambient", "Oct-warmed", "Dec-ambient", "Dec-warmed", 
                                    "Jan-ambient", "Jan-warmed", "Feb-ambient", "Feb-warmed",
                                    "Mar-ambient","Mar-warmed", 'Apr-ambient', 'Apr-warmed'))
 
-monthlab <- c("October", "December", "January", "February", "March", 'April')
+monthlab <- c("Oct", "Dec", "Jan", "Feb", "Mar", 'Apr')
 
 
 #bar plot by month and treatment
 
-windows(12, 6)
+windows(7, 10)
 palette(c("blue", "red"))
-par(mfrow=c(2, 1), mar=c(0,5,0.5,0.5))
-boxplot(d13Cph ~ id, data = phl, col=palette(), outline=FALSE, at=c(1,2, 7,8, 10,11, 13,14, 16:17), xaxt='n',
-        ylab=expression(Phloem~{delta}^13*C~~('\211')), xlab='')
-axis(1, at=c(1.5, 7.5, 10.5, 13.5, 16.5), labels=monthlab)
+par(mfrow=c(2, 1), mar=c(0,6,5,0.5), las=1, cex=1.1)
+boxplot(d13Cph ~ id, data = phloem, col=palette(), outline=FALSE, xaxt='n',
+        ylab=expression(delta^13*C[ph]~~('\211')), xlab='', cex.lab=1.2, ylim=c(-33, -24.5))
 legend("bottomleft", c("Ambient", "Elevated"), pch=22, pt.bg=palette(),  bty='n', pt.cex=1.5)
+legend('topleft', legend = '(a)', text.font = 2, bty='n')
+par(mar=c(5, 6, 0, 0.5))
+boxplot(d13CAnet ~ id, data = phloem, col=palette(), outline=FALSE, xaxt='n',
+        ylab=expression(delta^13*C[Anet]~~('\211')), xlab='', cex.lab=1.2, ylim=c(-33, -24.5))
+axis(1, at=c(1.5, 3.5, 5.5, 7.5, 9.5, 11.5), labels=monthlab, cex.lab=1.2)
+legend('topleft', legend='(b)', text.font = 2, bty='n')
