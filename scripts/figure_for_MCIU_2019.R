b99df2adb1ef06d2d8e8fd98dd1c2f2f6c5671e3
Teresa E. Gimeno <tegimeno@gmail.com>
2019-10-14 15:30:25 +0200

    figure for Min

--- scripts/figure_for_MCIU_2019.R
+++ scripts/figure_for_MCIU_2019.R
@@ -8,33 +8,33 @@ windows(12,6)
 par(mfrow=c(1,2), las=1, cex=1.25, mar=c(5, 5, 0.5, 0))
 plot(subset(phl, month==myMon[1] & T_treatment=='ambient')[,'iWUEgeMD']~
        subset(phl, month==myMon[1] & T_treatment=='ambient')[,'iWUEleafAvg_corrMD'], pch=myChar[1],
-     col=scales::alpha('blue', 0.3), bg=scales::alpha('blue',0.3), axes = F,
+     col='white', bg='white', axes = F,
      xlab=expression(iWUE[Delta~leaf]~'-'~italic(A)/italic(g)[m]~(mu*mol~mol^-1)),
      ylab=expression(iWUE[ge]~(mu*mol~mol^-1)),
      xlim=c(45, 230), ylim=c(85, 251), cex.lab=1.3)
 axis(1, at=seq(50, 200, 50), labels = seq(50, 200, 50), las=1)
 axis(2, at=seq(100, 250, 50), labels = seq(100, 250, 50), las=1)
 box()
-for(i in 2:length(myMon)){
-  points(subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment == 'control')[,'iWUEgeMD']~
-           subset(phl, month==myMon[i] & T_treatment=='ambient'& W_treatment == 'control')[,'iWUEleafAvg_corrMD'],
-         pch=myChar[i], col=scales::alpha('blue', 0.3), bg=scales::alpha('blue',0.3))
-}
-for(i in 1:length(myMon)){
-  points(subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment == 'control')[,'iWUEgeMD']~
-           subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment == 'control')[,'iWUEleafAvg_corrMD'],
-         pch=myChar[i], col=scales::alpha('red', 0.3), bg=scales::alpha('red',0.3))
-}
-for(i in 4:length(myMon)){
-  points(subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment == 'drydown')[,'iWUEgeMD']~
-           subset(phl, month==myMon[i] & T_treatment=='ambient'& W_treatment == 'drydown')[,'iWUEleafAvg_corrMD'],
-         pch=myChar[i], col='blue', bg='white')
-}
-for(i in 4:length(myMon)){
-  points(subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment == 'drydown')[,'iWUEgeMD']~
-           subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment == 'drydown')[,'iWUEleafAvg_corrMD'],
-         pch=myChar[i], col='red', bg='white')
-}
+# for(i in 1:length(myMon)){
+#   points(subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment == 'control')[,'iWUEgeMD']~
+#            subset(phl, month==myMon[i] & T_treatment=='ambient'& W_treatment == 'control')[,'iWUEleafAvg_corrMD'],
+#          pch=myChar[i], col=scales::alpha('blue', 0.3), bg=scales::alpha('blue',0.3))
+# }
+# for(i in 1:length(myMon)){
+#   points(subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment == 'control')[,'iWUEgeMD']~
+#            subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment == 'control')[,'iWUEleafAvg_corrMD'],
+#          pch=myChar[i], col=scales::alpha('red', 0.3), bg=scales::alpha('red',0.3))
+# }
+# for(i in 4:length(myMon)){
+#   points(subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment == 'drydown')[,'iWUEgeMD']~
+#            subset(phl, month==myMon[i] & T_treatment=='ambient'& W_treatment == 'drydown')[,'iWUEleafAvg_corrMD'],
+#          pch=myChar[i], col='blue', bg='white')
+# }
+# for(i in 4:length(myMon)){
+#   points(subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment == 'drydown')[,'iWUEgeMD']~
+#            subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment == 'drydown')[,'iWUEleafAvg_corrMD'],
+#          pch=myChar[i], col='red', bg='white')
+# }
 iWUEdf <- as.data.frame(iWUEsumm)
 for(i in 1:length(myMon)){
   Hmisc::errbar(x=subset(iWUEdf, month==myMon[i] & T_treatment=='ambient')[,'iWUEleafAvgCorrMean'],
@@ -74,8 +74,9 @@ for(i in 1:length(myMon)){
            subset(iWUEdf, month==myMon[i] & T_treatment=='warmed')[,'iWUEleafAvgCorrSE'],
          length = 0.03, angle = 90, code = 2)
 }
-plotrix::ablineclip(lm(iWUEgeMD ~ iWUEleafAvg_corrMD, data=phl), x1 = min(phl$iWUEleafAvg_corrMD, na.rm = T),
-                    x2 = max(phl$iWUEleafAvg_corrMD, na.rm = T))
+plotrix::ablineclip(lm(iWUEgeMean ~ iWUEleafAvgCorrMean, data=iWUEdf),
+                    x1 = min(iWUEdf$iWUEleafAvgCorrMean, na.rm = T),
+                    x2 = max(iWUEdf$iWUEleafAvgCorrMean, na.rm = T), lwd=2)
 
 legend('topleft', legend=myMon, pch=myChar, cex=1.3, bty='n')
 legend('topright', legend=c(expression(Leaf~delta^13*C)), text.font = 2, cex = 1.3, bty = 'n', pch=NA)
@@ -84,32 +85,32 @@ legend('bottomright', legend=expression(italic(R)^2~'='~0.36), bty = 'n')
 par(las=1, cex=1.25, mar=c(5, 0, 0.5, 5))
 plot(subset(phl, month==myMon[1] & T_treatment=='ambient')[,'iWUEgeMD']~
        subset(phl, month==myMon[1] & T_treatment=='ambient')[,'iWUEph_corrMD'], pch=myChar[1],
-     col=scales::alpha('blue', 0.3), bg=scales::alpha('blue',0.3), axes = F,
+     col='white', bg='white', axes = F,
      xlab=expression(iWUE[Delta~ph]~'-'~italic(A)/italic(g)[m]~(mu*mol~mol^-1)),
      xlim=c(45, 230), ylim=c(85, 251), cex.lab=1.3)
 axis(1, at=seq(50, 200, 50), labels = seq(50, 200, 50), las=1)
 axis(4, at=seq(100, 250, 50), labels = seq(100, 250, 50), las=1)
 box()
-for(i in 2:length(myMon)){
-  points(subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment=='control')[,'iWUEgeMD']~
-           subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment=='control')[,'iWUEph_corrMD'],
-         pch=myChar[i], col=scales::alpha('blue', 0.3), bg=scales::alpha('blue',0.3))
-}
-for(i in 1:length(myMon)){
-  points(subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment=='control')[,'iWUEgeMD']~
-           subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment=='control')[,'iWUEph_corrMD'],
-         pch=myChar[i], col=scales::alpha('red', 0.3), bg=scales::alpha('red',0.3))
-}
-for(i in 4:length(myMon)){
-  points(subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment=='drydown')[,'iWUEgeMD']~
-           subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment=='drydown')[,'iWUEph_corrMD'],
-         pch=myChar[i], col=scales::alpha('blue', 0.3), bg=scales::alpha('white',0.3))
-}
-for(i in 4:length(myMon)){
-  points(subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment=='drydown')[,'iWUEgeMD']~
-           subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment=='drydown')[,'iWUEph_corrMD'],
-         pch=myChar[i], col=scales::alpha('red', 0.3), bg=scales::alpha('white',0.3))
-}
+# for(i in 1:length(myMon)){
+#   points(subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment=='control')[,'iWUEgeMD']~
+#            subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment=='control')[,'iWUEph_corrMD'],
+#          pch=myChar[i], col=scales::alpha('blue', 0.3), bg=scales::alpha('blue',0.3))
+# }
+# for(i in 1:length(myMon)){
+#   points(subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment=='control')[,'iWUEgeMD']~
+#            subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment=='control')[,'iWUEph_corrMD'],
+#          pch=myChar[i], col=scales::alpha('red', 0.3), bg=scales::alpha('red',0.3))
+# }
+# for(i in 4:length(myMon)){
+#   points(subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment=='drydown')[,'iWUEgeMD']~
+#            subset(phl, month==myMon[i] & T_treatment=='ambient' & W_treatment=='drydown')[,'iWUEph_corrMD'],
+#          pch=myChar[i], col=scales::alpha('blue', 0.3), bg=scales::alpha('white',0.3))
+# }
+# for(i in 4:length(myMon)){
+#   points(subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment=='drydown')[,'iWUEgeMD']~
+#            subset(phl, month==myMon[i] & T_treatment=='warmed' & W_treatment=='drydown')[,'iWUEph_corrMD'],
+#          pch=myChar[i], col=scales::alpha('red', 0.3), bg=scales::alpha('white',0.3))
+# }
 iWUEdf <- as.data.frame(iWUEsumm)
 for(i in 1:length(myMon)){
   Hmisc::errbar(x=subset(iWUEdf, month==myMon[i] & T_treatment=='ambient')[,'iWUEphCorrMean'],
@@ -149,8 +150,8 @@ for(i in 1:length(myMon)){
            subset(iWUEdf, month==myMon[i] & T_treatment=='warmed')[,'iWUEphCorrSE'],
          length = 0.03, angle = 90, code = 2)
 }
-plotrix::ablineclip(lm(iWUEgeMD ~ iWUEph_corrMD, data=phl),
-                    x1=min(phl$iWUEph_corrMD, na.rm=T), x2=max(phl$iWUEph_corrMD, na.rm=T))
+plotrix::ablineclip(lm(iWUEgeMean ~ iWUEphCorrMean, data=iWUEdf),
+                    x1=min(iWUEdf$iWUEphCorrMean, na.rm=T), x2=max(iWUEdf$iWUEphCorrMean, na.rm=T), lwd=2)
 legend('topright', legend=c(expression(Phloem~delta^13*C)), text.font = 2, cex = 1.3, bty = 'n')
 legend('topleft', legend = c('Amb', 'Warm','Control','Drought'), pch = c(rep(19, 3), 1),
        col = c('blue', 'red', 'darkgrey','black'), bty = 'n', cex = 1.3)
